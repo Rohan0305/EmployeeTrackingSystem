cd4339ac127d3ec74835eaf02476ac05562e4a3c
Rohan <rohanvallapalli@gmail.com>
2024-09-13 00:42:52 -0400

    Test

--- EmployeeTrackingSystem/App.xaml.cs
+++ EmployeeTrackingSystem/App.xaml.cs
@@ -1,13 +1,29 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
 
-namespace EmployeeTrackingSystem;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
-}
+    public static ServiceProvider ServiceProvider { get; private set; }
 
+    public App()
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        services.AddDbContext<EmployeeContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("EmployeeDB")));
+    }
+}
---
+++ EmployeeTrackingSystem/Models/EmployeeContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+public class EmployeeContextFactory : IDesignTimeDbContextFactory<EmployeeContext>
+{
+    public EmployeeContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<EmployeeContext>();
+        var connectionString = configuration.GetConnectionString("EmployeeDB");
+
+        optionsBuilder.UseSqlServer(connectionString);
+
+        return new EmployeeContext(optionsBuilder.Options);
+    }
+}
